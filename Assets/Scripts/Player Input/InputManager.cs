580f2783fa3356902d9c1cc673e0c7a1822e6664
Raaee <99899402+Raaee@users.noreply.github.com>
2024-07-28 02:02:40 -0400

    player input + movement complete

---
+++ Assets/Scripts/Player Input/InputManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public InputAction movement;
+    public InputAction shield;
+    [Header("Player Inputs")] public PlayerControls playerControls;
+
+    [SerializeField] private float timeBetweenMoves = 0.1f;
+    private bool moveHeld = false;
+    private bool alreadyHeld = false;
+    private Vector2 currMoveDir;
+
+    [HideInInspector] public UnityEvent OnShieldUse;
+    [HideInInspector] public UnityEvent<Vector2> OnMovement;
+    [HideInInspector] public UnityEvent OnMoving; // Use this for audio stuf
+    private void Awake() {
+        playerControls = new PlayerControls();
+    }
+    private void Update() {
+        shield.performed += UseShield;
+        movement.canceled += ReleasingMove;
+        movement.started += HoldingMove;
+        movement.performed += Move;
+    }
+    private void OnEnable() {
+        movement = playerControls.Player.Movement;
+        movement.Enable();
+        shield = playerControls.Player.Shield;
+        shield.Enable();
+    }
+    private void OnDisable() {
+        shield.performed -= UseShield;
+        movement.canceled -= ReleasingMove;
+        movement.started -= HoldingMove;
+        movement.performed -= Move;
+    }
+    public void DisableControls() {
+        movement.Disable();
+        shield.Disable();
+    }
+    public void Move(InputAction.CallbackContext context) {
+        if (alreadyHeld) return;
+        currMoveDir = movement.ReadValue<Vector2>();
+        StartCoroutine(Moving());
+    }
+    public void HoldingMove(InputAction.CallbackContext context) {
+        moveHeld = true;
+    }
+    public void ReleasingMove(InputAction.CallbackContext context) {
+        moveHeld = false;
+    }
+    public void UseShield(InputAction.CallbackContext context) {
+        Debug.Log("Used Shield");
+        OnShieldUse.Invoke();
+    }
+    // to allow click and hold movement
+    private IEnumerator Moving() {
+        while (moveHeld) {
+            alreadyHeld = true;
+            yield return new WaitForSeconds(timeBetweenMoves);
+            OnMovement.Invoke(currMoveDir);
+            OnMoving.Invoke();
+        }
+        alreadyHeld = false;
+    }
+}
---
+++ Assets/Scripts/Player Input/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /**
+     * :)
+     * UP -> W / Up Arrow
+     * DOWN -> S / Down Arrow
+     * Click or Hold
+     */
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float moveDistance = 1f;
+    [SerializeField] private float upperPositionConstraint = 8f;
+    [SerializeField] private float lowerPositionConstraint = -8f;
+
+    private Vector2 moveInput;
+    private void Start() 
+    {inputManager.OnMovement.AddListener(MovePlayer);}
+    public void MovePlayer(Vector2 inputDirection) 
+    {moveInput = inputDirection;  
+    if (moveInput == Vector2.up) 
+    {MovePlayerUp(moveDistance);}
+    else if (moveInput == Vector2.down) 
+    {MovePlayerDown(moveDistance);}}
+    // In case we wanna use this for a debuff later:
+    public void MovePlayerUp(float distanceToMove)
+    {if (transform.position.y + moveDistance > upperPositionConstraint) 
+    {transform.position = new Vector3(transform.position.x, upperPositionConstraint, transform.position.z);
+    }else 
+    {transform.position = new Vector3(transform.position.x, transform.position.y + distanceToMove, transform.position.z);
+    }}
+    public void MovePlayerDown(float distanceToMove) 
+    {if (transform.position.y - moveDistance < lowerPositionConstraint) 
+    {transform.position = new Vector3(transform.position.x, lowerPositionConstraint, transform.position.z);
+    }else 
+    {transform.position = new Vector3(transform.position.x, transform.position.y - distanceToMove, transform.position.z);}}
+}
