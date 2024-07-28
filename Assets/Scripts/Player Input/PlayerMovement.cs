bca211c222be085d3b4ed66a0128fc7caf44692e
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-07-28 07:56:49 -0400

    move prefab folder & reformat code

--- Assets/Scripts/Player Input/PlayerMovement.cs
+++ Assets/Scripts/Player Input/PlayerMovement.cs
@@ -11,29 +11,55 @@ public class PlayerMovement : MonoBehaviour
      * Click or Hold
      */
     [SerializeField] private InputManager inputManager;
+
     [SerializeField] private float moveDistance = 1f;
     [SerializeField] private float upperPositionConstraint = 8f;
     [SerializeField] private float lowerPositionConstraint = -8f;
 
     private Vector2 moveInput;
-    private void Start() 
-    {inputManager.OnMovement.AddListener(MovePlayer);}
-    public void MovePlayer(Vector2 inputDirection) 
-    {moveInput = inputDirection;  
-    if (moveInput == Vector2.up) 
-    {MovePlayerUp(moveDistance);}
-    else if (moveInput == Vector2.down) 
-    {MovePlayerDown(moveDistance);}}
+
+    private void Start()
+    {
+        inputManager.OnMovement.AddListener(MovePlayer);
+    }
+
+    public void MovePlayer(Vector2 inputDirection)
+    {
+        moveInput = inputDirection;
+        if (moveInput == Vector2.up)
+        {
+            MovePlayerUp(moveDistance);
+        }
+        else if (moveInput == Vector2.down)
+        {
+            MovePlayerDown(moveDistance);
+        }
+    }
+
     // In case we wanna use this for a debuff later:
     public void MovePlayerUp(float distanceToMove)
-    {if (transform.position.y + moveDistance > upperPositionConstraint) 
-    {transform.position = new Vector3(transform.position.x, upperPositionConstraint, transform.position.z);
-    }else 
-    {transform.position = new Vector3(transform.position.x, transform.position.y + distanceToMove, transform.position.z);
-    }}
-    public void MovePlayerDown(float distanceToMove) 
-    {if (transform.position.y - moveDistance < lowerPositionConstraint) 
-    {transform.position = new Vector3(transform.position.x, lowerPositionConstraint, transform.position.z);
-    }else 
-    {transform.position = new Vector3(transform.position.x, transform.position.y - distanceToMove, transform.position.z);}}
+    {
+        if (transform.position.y + moveDistance > upperPositionConstraint)
+        {
+            transform.position = new Vector3(transform.position.x, upperPositionConstraint, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + distanceToMove,
+                transform.position.z);
+        }
+    }
+
+    public void MovePlayerDown(float distanceToMove)
+    {
+        if (transform.position.y - moveDistance < lowerPositionConstraint)
+        {
+            transform.position = new Vector3(transform.position.x, lowerPositionConstraint, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - distanceToMove,
+                transform.position.z);
+        }
+    }
 }
