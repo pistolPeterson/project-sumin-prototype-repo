abac6c82d42115bdb782e668e1d252e0402bf727
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-08-02 01:23:54 -0400

    clean up (#10)

--- Assets/Scripts/Boss/AttackPattern.cs
+++ Assets/Scripts/Boss/AttackPattern.cs
@@ -1,37 +1,44 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using com.cyborgAssets.inspectorButtonPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public abstract class AttackPattern : MonoBehaviour
 {
     [SerializeField] protected GameObject projectilePrefab;
     [SerializeField] protected float playFieldPosConstraint = 8f;
-    [SerializeField] protected float attackUptime = 5f; // how long an attack lasts for
+    [SerializeField] private float attackDuration = 5f; // how long an attack lasts for
     [SerializeField] protected float delayBetweenAttacks = 0.5f; // delay time before calling the next attack
     protected Vector3 projectileSpawnLoc;
-    protected float uptimeTimer = 0f;
+    private float timer = 0f;
 
-    private void Start() {
+    protected virtual void Start() {
         projectileSpawnLoc = transform.position;
+        
     }
     public abstract void Attack();
     public void SetSpawnLoc(float yPos) {
         projectileSpawnLoc = new Vector3(transform.position.x, yPos, transform.position.z);
     }
     public void StartAttack() {
-        StartCoroutine(UptimeAttack());
+       
+        StartCoroutine(AttackLoop());
     }
-    public void StopAttack() {
-        StopCoroutine(UptimeAttack());
-        uptimeTimer = attackUptime;
+    [ProButton]
+    private void StopAttack() {
+        StopCoroutine(AttackLoop());
+        timer = attackDuration;
     }
-    private IEnumerator UptimeAttack() {
-        uptimeTimer = 0f;
-        while (uptimeTimer < attackUptime) {
-            Debug.Log("Attacking Time remaining: " + (attackUptime - uptimeTimer));
+    
+    private IEnumerator AttackLoop() {
+        timer = 0f;
+      
+        while (timer < attackDuration) {
+            //Debug.Log("Attacking Time remaining: " + (attackDuration - timer));
             Attack();
             yield return new WaitForSeconds(delayBetweenAttacks);
-            uptimeTimer += Time.fixedDeltaTime + delayBetweenAttacks; // to normalize the time
+            timer += Time.fixedDeltaTime + delayBetweenAttacks; // to normalize the time
         }
     }
 }
--- Assets/Scripts/Boss/RandomAttackPattern.cs
+++ Assets/Scripts/Boss/RandomAttackPattern.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RandomAttackPattern : AttackPattern
 {
+    protected override void Start()
+    {
+        base.Start();
+    }
+
     public override void Attack() {
         SpawnRandomProjectile();
     }
