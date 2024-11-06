376bf074e16cf7717fb377b05f63ccf31b80a0df
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-11-06 18:23:17 -0500

    restart shield

--- Assets/Scripts/Player/Shield.cs
+++ Assets/Scripts/Player/Shield.cs
@@ -2,95 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class Shield : MonoBehaviour {
     
     [SerializeField] private InputManager input;
-    [SerializeField] private PlayerHealth hp;
-    [SerializeField] private float maxCharge = 10f;
-    [SerializeField] private float shieldCost = 1f;
-    [SerializeField] private float durationTillRegen = 1f;
-    [SerializeField] private float regenDelay = 0.5f;
-    [SerializeField] private float regenRate = 1f;
+     [SerializeField] private PlayerHealth playerHealth;
 
-    [Header("Debug")]
-    [SerializeField] private float currentCharge;
-    [SerializeField] private float timeWithoutUse = 0f;
-    [SerializeField] private bool usingShield = false;
-    [SerializeField] private bool canUse = true;
-
-    [HideInInspector] public UnityEvent<float> OnShieldUse; // to update UI
-
-    private void Start() {
-        input.OnShieldUse.AddListener(UseShield);
-        input.OnShieldHeld.AddListener(IsUsingShield);
-        currentCharge = maxCharge;
-        timeWithoutUse = 0f;
+    public void EnableShield()
+    {
+        //logic 
+        playerHealth.SetInvincibility(true);
+        //visual
     }
-    /*
-     * Logic:
-     * - press or hold to use
-     * - when release, start timer that counts to the time
-     * - when it reaches the time, regenerate health
-     * - during regen, cannot use until full
-     */
-    private void Update() {
-        if ((usingShield && currentCharge != 0) || ChargeAvailable()) {
-            timeWithoutUse = 0f;
-            return;
-        }
-        hp.SetInvincibility(false);
-        timeWithoutUse += Time.deltaTime;
-        if (timeWithoutUse >= durationTillRegen) {
-            if (canUse)
-            StartCoroutine(RegenerateShield());
-        }
-    }
-    public IEnumerator RegenerateShield() {
-        canUse = false;
-        while (currentCharge < maxCharge && !canUse) {
-            AddCharge(regenRate);
-            OnShieldUse.Invoke(currentCharge);
-            yield return new WaitForSeconds(regenDelay);
-        }
-    }
-    public bool ChargeAvailable() {
-        return currentCharge >= shieldCost;
-    }
-    public void IsUsingShield(bool shielding) {
-        usingShield = shielding;
-    }
-    public void UseShield() {
-        if (!canUse) return;
-        OnShieldUse.Invoke(currentCharge);
-        if (shieldCost > currentCharge) {
-            Debug.Log("Not enough charge");
-            timeWithoutUse = durationTillRegen-1; // to stop usage when at 0 charge
-            return;
-        }
-        hp.SetInvincibility(true);
-        RemoveCharge(shieldCost);
-    }
-    public void AddCharge(float amt) {
-        currentCharge += amt;
-        if (currentCharge >= maxCharge) {
-            currentCharge = maxCharge;
-            canUse = true;
-        }
-    }
-    public void RemoveCharge(float amt) {
-        currentCharge -= amt;
-        if (currentCharge < 0) {
-            currentCharge = 0;
-        }
-    }
-    public float GetMaxShieldCharge() {
-        return maxCharge;
-    }
-    public bool GetUsingShield() {
-        return usingShield;
-    }
-    public bool GetCanUseShield() {
-        return canUse;
+    
+    public void DisableShield()
+    {
+        //logic
+        playerHealth.SetInvincibility(false);
+        //visual
     }
 }
--- Assets/Scripts/Player/ShieldVisual.cs
+++ Assets/Scripts/Player/ShieldVisual.cs
@@ -4,53 +4,22 @@ using System.Collections.Generic;
 using com.cyborgAssets.inspectorButtonPro;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ShieldVisual : MonoBehaviour
 {
-    private SpriteRenderer sr;
     private Shield shield;
-    //[Header("mind you business rae")]
-    //public Color[] colorPallete; 
-   // private int colorPalleteIndex = 1; //skipping one since its same color as background
- //   private float colorChangeTime = 0.125f;
 
-    [SerializeField] private GameObject shieldVisual; 
+     [SerializeField] private GameObject shieldVisualGO; 
 
-    private void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-        shield = GetComponentInParent<Shield>();
-    }
-    private void Update() {
-        if (shield.GetUsingShield() && shield.GetCanUseShield()) {
-            ShowShield();
-        }
-        else CloseShield();
-    }
-    /*private void ShieldSpawn() //shield showcase at the begining of the round, just for some cool points. can be 
-    {
-        //go through all colors of pallete...
-        sr.DOColor(colorPallete[colorPalleteIndex], colorChangeTime).OnComplete(NextColor);
-        //whiles spinning...
-        //and getting bigger...
-    }
-
-    private void NextColor()
-    {
-        colorPalleteIndex++;
-        if(colorPalleteIndex >= colorPallete.Length)
-            return;
-      
-        sr.DOColor(colorPallete[colorPalleteIndex], colorChangeTime).OnComplete(NextColor);
-        
-    }*/
+  
     private void ShowShield()
     {
-        shieldVisual.SetActive(true);
+        shieldVisualGO.SetActive(true);
     }
     private void CloseShield()
     {
-        shieldVisual.SetActive(false);
+        shieldVisualGO.SetActive(false);
     }
 
 }
