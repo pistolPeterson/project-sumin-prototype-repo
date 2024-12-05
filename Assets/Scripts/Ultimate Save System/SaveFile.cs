39cba7b09fc902b3e40e2fa25dcaff8e33f15a8a
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-12-05 18:58:16 -0500

    save system commit

---
+++ Assets/Scripts/Ultimate Save System/SaveFile.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SaveFile
+{
+    public List<NodeEnum> mapNodeEnums = new List<NodeEnum>();
+
+    public int nodeCount;
+    public int currentNodeId;
+
+    public override string ToString()
+    {
+        var nodeEnumListStr = $"---Map Node Enums (Count:{mapNodeEnums.Count})---\n";
+
+        for (var i = 0; i < mapNodeEnums.Count; i++){
+            nodeEnumListStr += $"Node {i}:" + mapNodeEnums[i] + "\n";
+        }
+
+        return nodeEnumListStr + $"\nnodeCount:{nodeCount}\ncurrentNodeId:{currentNodeId}";
+    }
+}
